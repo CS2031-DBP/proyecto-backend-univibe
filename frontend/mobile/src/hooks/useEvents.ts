205ed0b114f520a47e60abc797f0e82178b56093
nvm230 <marckantonio2005@gmail.com>
2025-11-25 21:05:26 -0500

    Univibe22.4.3-bf2

--- frontend/mobile/src/hooks/useEvents.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchEvents } from '@/services/eventService';
-import { PaginatedResponse, Event } from '@/types';
-
-export const useEvents = (filters: { page?: number; size?: number; status?: string; search?: string }) =>
-  useQuery<PaginatedResponse<Event>>({
-    queryKey: ['events', filters],
-    queryFn: ({ signal }) => fetchEvents(filters, signal)
-  });
